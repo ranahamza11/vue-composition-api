0a47628fb128f98d1ee4d4fa89f5b3ee3fe916cb
Muhammad Hamza Naeem <drovisfrovis@gmail.com>
2022-09-06 13:54:25 +0500

    initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {useFlash} from "@/composables/useFlash";
+let { flash } = useFlash();
+</script>
+
+<template>
+  <div>
+    <p>
+      <button @click="flash('salam','it works on the about page')">Click Me</button>
+    </p>
+  </div>
+</template>
+
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+// import flash from "@/mixins/flash";
+import {useFlash} from "@/composables/useFlash";
+let { flash } = useFlash();
+
+
+</script>
+
+<template>
+  <main>
+    <p>
+      <button @click="flash('test','it works', 'info')">Click Me</button>
+    </p>
+
+  </main>
+</template>
